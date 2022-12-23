3322f3a17a164955966bfb540fc24c796b75cee9
fabiosantos- <fabio.aes@gmail.com>
2022-12-23 10:41:04 -0400

    new repo

---
+++ script/script.js
@@ -0,0 +1,19 @@
+// SELECTORES
+const texto = document.querySelector("main>p");
+const span = document.querySelector("main>h6>span");
+const btn = document.querySelector("button");
+//
+
+// API URL
+const urlApi = "https://api.adviceslip.com/advice";
+//
+
+// EVENT
+btn.addEventListener("click", () => {
+  fetch(urlApi).then(function (response) {
+    response.json().then(function (data) {
+      span.innerText = ` #${data.slip.id}`;
+      texto.innerText = `"${data.slip.advice}"`;
+    });
+  });
+});
